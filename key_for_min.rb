9392cb3606a51beea5dc211d036ddced060fae10
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-09 01:19:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,8 @@ require 'pry'
 
 
 def key_for_min_value(name_hash)
-  #hash_array = []
   lowest = nil 
   name_hash.each do |key, value|
-    #hash_array << { key => value }
     if lowest == nil
       lowest = value
     elsif lowest > value
