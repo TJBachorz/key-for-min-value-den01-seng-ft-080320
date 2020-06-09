db3da2b59e91888fe015c13e2dfd001927aa34af
Thomas Bachorz <tjbachorz@gmail.com>
2020-06-09 01:09:04 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,7 @@ require 'pry'
 
 
 def key_for_min_value(name_hash)
-  hash_array = []
-  lowest = nil 
   name_hash.each do |key, value|
-    binding.pry
-    hash_array << { key => value }
-  end
+    puts key, value
 end
     
